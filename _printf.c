1d71b77132358497dd953b6d3a2e15c369989e1b
Codedjay672 <emmajoel6456@gmail.com>
2022-08-07 19:17:13 +0000

    created _printf.c

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf -> prints anything on the atdout
+ * @format: string literal for formating text input
+ * Return: returns integer value of all characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int printed;
+	toPrint func[] = {
+		{"c", pchar},
+		{"s", pstr},
+		{"%", percent},
+		{"d", pinteger},
+		{"i", pinteger},
+		{NULL, NULL}
+	};
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	va_stat(args, format);
+	printed = parse(format, func, args);
+	va_end(args);
+	return (printed);
+}
