5eb5b238e24b375256f56ccade0e08569a54d7b2
Codedjay672 <emmajoel6456@gmail.com>
2022-08-07 18:19:44 +0000

    main.h created

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stlib.h>
+#include <stdarg.h>
+
+/**
+ * struct forprintf -> structure contrary functions
+ *
+ * @operator: the operator
+ * @f: function
+ */
+
+struct forPrintf
+{
+	char *operation;
+	int (*f)(va_list);
+};
+typedef struct forPrintf toPrint;
+int _printf(const char *format, ...);
+int _putchar(char);
+int pchar(va_list);
+int pstr(va_list);
+int percent(va_list);
+int pinteger(va_list);
+int number(va_list);
+int parse(const char *format, toPrint func[], va_list args);
+
+#endif
