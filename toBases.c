f0781b94df946a93085f0221e78b3b0f2f948197
Codedjay672 <emmajoel6456@gmail.com>
2022-08-10 10:30:49 +0000

    updated toBases.c

--- toBases.c
+++ toBases.c
@@ -2,7 +2,6 @@
 /**
  * print_binary - prints a number in base 2
  * @l: va_list arguments from printf
- * @f: pointer to struct
  * Description: function calls toPrint(0
  * which in turn  converts input
  * Return: number of chars printed
@@ -10,8 +9,8 @@
 int print_binary(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-
 	char *str = convert(num, 2, 0);
+
 	return (_puts(str));
 }
 
@@ -19,7 +18,6 @@ int print_binary(va_list l)
  * print_hex - prints in hexadecimal base
  * in lowercase
  * @l: va_list arguments from _printf
- * @f: pointer to strtucture
  * Description: the function calls convert() which
  * converts input to correct base and returns
  * a string
@@ -28,9 +26,9 @@ int print_binary(va_list l)
 int print_hex(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-
 	char *str = convert(num, 16, 1);
 	int count = 0;
+
 	count += _puts(str);
 	return (count);
 }
@@ -39,16 +37,15 @@ int print_hex(va_list l)
  * print_hex_big - prints a number in hexadecimal base
  * in uppercase
  * @l: va_list argument
- * @f: pointer to struct
  * Description: the function calls convert
  * Return: the number of chars printed
  */
 int print_hex_big(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-
 	char *str = convert(num, 16, 0);
 	int count = 0;
+
 	count += _puts(str);
 	return (count);
 }
@@ -56,16 +53,15 @@ int print_hex_big(va_list l)
 /**
  * print_octal - prints number in base 8
  * @l: va_list argument
- * @f: pointer to struct
  * Description: function calls convert()
  * Return: number of printer chars
  */
 int print_octal(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-
 	char *str = convert(num, 8, 0);
 	int count = 0;
+
 	count += _puts(str);
 	return (count);
 }
