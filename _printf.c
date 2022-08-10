92e8fb2fca71f653c1b5f8230e6fac31ea6ef7af
Codedjay672 <emmajoel6456@gmail.com>
2022-08-10 10:42:03 +0000

    updated _printf

--- _printf.c
+++ _printf.c
@@ -8,22 +8,31 @@
 
 int _printf(const char *format, ...)
 {
-	int printed;
-	toPrint func[] = {
-		{"c", pchar},
-		{"s", pstr},
-		{"%", percent},
-		{"d", pinteger},
-		{"i", pinteger},
-		{NULL, NULL}
-	};
-	va_list args;
+int printed;
+toPrint func[] = {
+	{"c", pchar},
+	{"s", pstr},
+	{"%", percent},
+	{"d", pinteger},
+	{"i", pinteger},
+	{"b", print_binary},
+	{"x", print_hex},
+	{"X", print_hex_big},
+	{"o", print_octal},
+	{"u", print_unsigned},
+	{"R", print_rot13},
+	{"r", print_rev},
+	{"S", print_bigS},
+	{"p", print_address},
+	{NULL, NULL}
+};
+va_list args;
 
-	if (format == NULL)
-		return (-1);
+if (format == NULL)
+    return (-1);
 
-	va_start(args, format);
-	printed = parse(format, func, args);
-	va_end(args);
-	return (printed);
+va_start(args, format);
+printed = parse(format, func, args);
+va_end(args);
+return (printed);
 }
