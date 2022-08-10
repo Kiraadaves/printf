4d049a5e5f8ebe6e6feec0d7e4bd9ba9ecaa9f25
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-10 03:07:33 -0700

    to bases

---
+++ toBases.c
@@ -0,0 +1,72 @@
+#include "main.h"
+/**
+ * print_binary - prints a number in base 2
+ * @l: va_list arguments from printf
+ * @f: pointer to struct
+ * Description: function calls toPrint(0
+ * which in turn  converts input
+ * Return: number of chars printed
+ */
+int print_binary(va_list l)
+{
+	unsigned int num = va_arg(l, unsigned int);
+
+	char *str = convert(num, 2, 0);
+	return (_puts(str));
+}
+
+/**
+ * print_hex - prints in hexadecimal base
+ * in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to strtucture
+ * Description: the function calls convert() which
+ * converts input to correct base and returns
+ * a string
+ * Return: number of char printed
+ */
+int print_hex(va_list l)
+{
+	unsigned int num = va_arg(l, unsigned int);
+
+	char *str = convert(num, 16, 1);
+	int count = 0;
+	count += _puts(str);
+	return (count);
+}
+
+/**
+ * print_hex_big - prints a number in hexadecimal base
+ * in uppercase
+ * @l: va_list argument
+ * @f: pointer to struct
+ * Description: the function calls convert
+ * Return: the number of chars printed
+ */
+int print_hex_big(va_list l)
+{
+	unsigned int num = va_arg(l, unsigned int);
+
+	char *str = convert(num, 16, 0);
+	int count = 0;
+	count += _puts(str);
+	return (count);
+}
+
+/**
+ * print_octal - prints number in base 8
+ * @l: va_list argument
+ * @f: pointer to struct
+ * Description: function calls convert()
+ * Return: number of printer chars
+ */
+int print_octal(va_list l)
+{
+	unsigned int num = va_arg(l, unsigned int);
+
+	char *str = convert(num, 8, 0);
+	int count = 0;
+	count += _puts(str);
+	return (count);
+}
+
